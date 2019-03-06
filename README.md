136638d0c26628833943f72045c420f1ce5d688d
manish <manishrawat9992@gmail.com>
2019-03-07 01:17:18 +0530

    player movement added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SaladChef
 
+Unity Version: 2018.3.4f1
+
+Making a local multiplayer co-op game I took liberty to make the timer and score common for both player so they work together to gain higher score together.
---
+++ UnityProject-SaladChef/Assets/Scripts/playerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speedA, speedB;
+
+    private Vector2 moveVelocityA, moveVelocityB;
+
+    [SerializeField]
+    Rigidbody2D rbA, rbB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputValA = new Vector2(Input.GetAxisRaw("HorizontalA"), Input.GetAxisRaw("VerticalA"));
+        moveVelocityA = inputValA.normalized * speedA;
+
+        Vector2 inputValB = new Vector2(Input.GetAxisRaw("HorizontalB"), Input.GetAxisRaw("VerticalB"));
+        moveVelocityB = inputValB.normalized * speedA;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rbA.MovePosition(rbA.position + moveVelocityA * Time.fixedDeltaTime);
+        rbB.MovePosition(rbB.position + moveVelocityB * Time.fixedDeltaTime);
+    }
+}
